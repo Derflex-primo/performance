c9a0c8bb1455c6144546a83ef169e51ee84aa686
Derflex-primo <dryldeogracias@gmail.com>
2024-07-07 23:54:48 +0800

    feat: nav -> include web-blueprint title

--- app/components/Portfolio.tsx
+++ app/components/Portfolio.tsx
@@ -3,9 +3,10 @@ import Container from "./Container";
 
 const Portfolio = () => {
   return (
-    <Container class="h-[100vh] w-full mt-[80px]">
-      <div className="w-full flex text-center justify-center ">
+    <Container class="h-auto w-full mt-[80px]">
+      <div className="flex flex-col w-full text-center justify-center animate-pulse border-stone-800 border-[1px] py-12">
         <h2 className="text-2xl font-semibold">Portfolio</h2>
+        <p className="p-4">Still in progress. Be the first one!</p>
       </div>
     </Container>
   );
