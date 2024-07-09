4f9f8bce051e25a0911d07d5126b421391e21661
Derflex-primo <dryldeogracias@gmail.com>
2024-07-09 22:13:04 +0800

    new theme

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -4,8 +4,8 @@ import Container from './Container'
 const Home = () => {
   return (
     <Container layout='flex-col' class="h-[100vh] w-full items-center justify-center">
-        <h1 className='leading-relaxed md:leading-relaxed text-pretty md:text-balance  text-4xl md:text-6xl extra-xs:pl-8 xs:pl-0 text-start md:text-center font-semibold mb-[180px]'>
-           Looking to truly <span className='bg-blue-500 px-1.5'>stand out?</span> It&apos;s essential <br className='hidden md:block' /> to do more than just show up online.   
+        <h1 className='leading-relaxed md:leading-relaxed text-pretty md:text-balance  text-4xl md:text-6xl extra-xs:pl-8 xs:pl-0 text-start md:text-center font-semibold'>
+           Looking to truly <span className='bg-blue-500 bg-opacity-30 px-1.5'>stand out?</span> It&apos;s essential <br className='hidden md:block' /> to do more than just show up online.   
         </h1>
     </Container>
   )
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,17 +4,19 @@ import Image from "next/image";
 import daryl from "../../public/json/daryl.json";
 import Link from "next/link";
 import Menu from "./Menu";
+import Support from "@/public/icons/Support";
 
 const Navbar = () => {
   return (
     <>
-      <div className="fixed w-full top-0 z-[10000] flex justify-center p-2 text-xs sm:text-sm bg-green-500">
-        Hello there , im giving 50% discount for my first client 💪!
-      </div>
-      <Container>
-        <nav className="flex flex-row w-full md:items-center justify-between  py-4 sm:py-8 mt-8 sm:mt-5">
-          <div className="flex  md:items-center space-x-2 md:space-x-6">
-            <section className="relative h-10 w-10  rounded-xl">
+      <Container class="fixed w-full sm:py-3 bg-black border-b-[0.5px] border-stone-800">
+        <nav className="flex flex-row w-full items-center justify-between    ">
+          <div className="flex items-center space-x-1 md:space-x-3">
+            <div className="flex items-center text-2xl sm:text-3xl font-bold text-white mr-2 sm:pr-6 sm:border-r-[0.5px] border-stone-800">
+              kanlura
+              <span className="text-blue-500">n</span>
+            </div>
+            <section className="invisible sm:visible relative h-8 w-8 rounded-xl">
               <Image
                 fill
                 src="/images/mainlogo.jpeg"
@@ -25,12 +27,12 @@ const Navbar = () => {
             <Link
               href="https://twitter.com/dryldeogracias"
               target="blank"
-              className="group cursor-pointer mb-2 sm:mb-0"
+              className="group cursor-pointer mb-2 sm:mb-0 invisible sm:visible"
             >
-              <h1 className="font-bold  text-[14px] xs:text-md sm:text-lg">
+              <h1 className="font-bold text-stone-100  text-[14px] xs:text-md sm:text-sm">
                 {daryl.name}
               </h1>
-              <p className="text-xs text-gray-600 group-hover:underline">
+              <p className="text-[9px] text-gray-600 group-hover:underline">
                 @dryldeogracias
               </p>
             </Link>
@@ -38,15 +40,16 @@ const Navbar = () => {
           <div className="flex md:hidden">
             <Menu />
           </div>
-          <div className="hidden space-x-4 md:flex">
-            <h3 className="font-semibold cursor-pointer px-2.5 py-1.5 hover:bg-white hover:bg-opacity-[0.07] text-white transition-colors ease-in-out rounded underline underline-offset-2 decoration-red-400">
+          <div className="hidden space-x-6 md:flex items-center text-sm">
+            <h3 className="font-semibold cursor-pointer hover:bg-white hover:bg-opacity-[0.07] text-white transition-colors ease-in-out rounded underline underline-offset-2 decoration-red-400">
               Development
             </h3>
-            <h3 className="font-semibold cursor-pointer px-2.5 py-1.5 hover:bg-white hover:bg-opacity-[0.07] text-white transition-colors ease-in-out rounded">
-              Web-Blueprint
+            <h3 className="font-semibold cursor-pointer  hover:bg-white hover:bg-opacity-[0.07] text-white transition-colors ease-in-out rounded">
+              Web Blueprint
             </h3>
-            <h3 className="font-semibold cursor-pointer px-2.5 py-1.5 bg-blue-500 hover:bg-blue-400 text-white transition-colors ease-in-out rounded">
-              Contact us
+            <h3 className="flex items-center space-x-1.5 font-semibold cursor-pointer px-1.5 py-0.5 bg-blue-500 hover:bg-blue-400 text-white transition-colors ease-in-out rounded-[2px]">
+              <span>Contact us</span>
+              <Support />
             </h3>
           </div>
         </nav>
